4c94039f8b292f008dee34da08547ba2e75b6162
zhulinyv <zhulinyv2005@outlook.com>
2024-07-29 22:41:08 +0800

    :tada: 首次提交

--- __init__.py
+++ __init__.py
@@ -1,36 +1,33 @@
-from pathlib import Path
-
 import gradio as gr
 
-from plugins.t2i.sanp_plugin_template.utils import t2i
-from utils.utils import open_folder
-
-
-def plugin_template(name="模板插件", description="描述", func=None):
-    with gr.Tab(name):
-        with gr.Row():
-            with gr.Column(scale=8):
-                gr.Markdown(f"> {description}")
-            folder = gr.Textbox(Path("./output/t2i"), visible=False)
-            open_folder_ = gr.Button("打开保存目录", scale=1)
-            open_folder_.click(open_folder, inputs=folder)
-        with gr.Row():
-            generate_button = gr.Button("无限生成")
-            stop_button = gr.Button("停止生成")
-        otp_img = gr.Image()
-        cancel_event = otp_img.change(
-            fn=func,
-            inputs=None,
-            outputs=otp_img,
-            show_progress="hidden",
-        )
-        generate_button.click(
-            fn=func,
-            inputs=None,
-            outputs=otp_img,
-        )
-        stop_button.click(None, None, None, cancels=[cancel_event])
+from plugins.webui.sanp_plugin_image_compression.utils import (
+    compression_and_organization,
+    image_compression,
+    image_organization,
+)
 
 
 def plugin():
-    plugin_template("模板插件", "这是一段描述说明或教程", t2i)
+    with gr.Tab("压缩整理"):
+        image_path = gr.Textbox("", label="图片目录")
+        image_format = gr.Radio(
+            ["jpg", "png"],
+            label="压缩格式(jpg 格式图片较小, 但无法存储 pnginfo; png 格式反之)",
+        )
+        with gr.Row():
+            compression_button = gr.Button("仅压缩")
+            organization_button = gr.Button("仅整理")
+            compression_and_organization_button = gr.Button("压缩并整理")
+        output_info = gr.Textbox(label="输出信息")
+
+        compression_button.click(
+            image_compression, inputs=[image_format, image_path], outputs=output_info
+        )
+        organization_button.click(
+            image_organization, inputs=image_path, outputs=output_info
+        )
+        compression_and_organization_button.click(
+            compression_and_organization,
+            inputs=[image_format, image_path],
+            outputs=output_info,
+        )
--- utils.py
+++ utils.py
@@ -1,42 +1,123 @@
-import random
+import cv2
+import openpyxl
+from loguru import logger
+from openpyxl.drawing.image import Image as OPXIMG
+from openpyxl.styles import Alignment
+from PIL import Image as PILIMG
 
-from src.t2i import t2i_by_hand
-from utils.env import env
-from utils.utils import format_str, sleep_for_cool
-
-
-def prompt():
-    positive = ...
-    negative = ...
-    return format_str(positive), format_str(negative)
+from utils.imgtools import return_pnginfo, revert_img_info
+from utils.utils import file_namel2pathl, file_path2dir, file_path2list
 
 
-def t2i():
-    positive, negative = prompt()
-    resolution = (
-        random.choice(["832x1216", "1024x1024", "1216x832"])
-        if env.img_size == -1
-        else "{}x{}".format(env.img_size[0], env.img_size[1])
+def image_organization(image_path):
+    logger.info("正在整理图片...")
+    image_list = file_namel2pathl(file_path2list(image_path), image_path)
+    iamge_number = len(image_list)
+
+    wb = openpyxl.Workbook()
+    ws = wb.active
+
+    ws.row_dimensions[1].height = 50
+
+    for row in range(2, iamge_number + 2):
+        ws.row_dimensions[row].height = 300
+
+    for col in ["A", "B", "C"]:
+        ws.column_dimensions[col].width = 40
+
+    for col in range(4, 12):
+        col_letter = openpyxl.utils.get_column_letter(col)
+        ws.column_dimensions[col_letter].width = 20
+
+    ws.append(
+        [
+            "图片",
+            "正面提示词",
+            "负面提示词",
+            "分辨率",
+            "采样步数",
+            "提示词相关性",
+            "噪声计划表",
+            "采样器",
+            "sm",
+            "sm_dyn",
+            "随机种子",
+        ]
     )
-    scale = env.scale
-    sampler = env.sampler
-    noise_schedule = env.noise_schedule
-    steps = env.steps
-    sm = env.sm
-    sm_dyn = env.sm_dyn
-    seed = random.randint(1000000000, 9999999999) if env.seed == -1 else env.seed
-    img = t2i_by_hand(
-        positive,
-        negative,
-        resolution,
-        scale,
-        sampler,
-        noise_schedule,
-        steps,
-        sm,
-        sm_dyn,
-        seed,
-        times=1,
-    )
-    sleep_for_cool(env.t2i_cool_time - 6, env.t2i_cool_time + 6)
-    return img
+
+    number = 2
+    for image in image_list:
+        with PILIMG.open(image) as pilimg:
+            (
+                positive_input,
+                negative_input,
+                resolution,
+                steps,
+                scale,
+                noise_schedule,
+                sampler,
+                sm,
+                sm_dyn,
+                seed,
+            ) = return_pnginfo(pilimg)
+
+            w, h = pilimg.size
+
+            image = OPXIMG(image)
+            image.width, image.height = 260, int(260 / w * h)
+
+            ws.add_image(image, f"A{number}")
+            ws[f"B{number}"] = positive_input
+            ws[f"C{number}"] = negative_input
+            ws[f"D{number}"] = resolution
+            ws[f"E{number}"] = steps
+            ws[f"F{number}"] = scale
+            ws[f"G{number}"] = noise_schedule
+            ws[f"H{number}"] = sampler
+            ws[f"I{number}"] = sm
+            ws[f"J{number}"] = sm_dyn
+            ws[f"K{number}"] = seed
+
+        number += 1
+
+    alignment = Alignment(horizontal="center", vertical="center", wrapText=True)
+    for row in ws.iter_rows():
+        for cell in row:
+            cell.alignment = alignment
+
+    wb.save(file_path2dir(image_path) + "/organization.xlsx")
+
+    logger.success("整理完成!")
+    return "整理完成!"
+
+
+def image_compression(format_, image_path):
+    image_list = file_namel2pathl(file_path2list(image_path), image_path)
+
+    for image in image_list:
+        logger.info(f"正在压缩{image}...")
+
+        cv2_image = cv2.imread(image)
+
+        if format_ == "jpg":
+            compression_params = [cv2.IMWRITE_JPEG_QUALITY, 90]
+        elif format_ == "png":
+            compression_params = [cv2.IMWRITE_PNG_COMPRESSION, 9]
+
+        cv2.imwrite(image[:-4] + f".{format}", cv2_image, compression_params)
+
+        if format_ == "png":
+            with PILIMG.open(image) as pil_img:
+                pnginfo = pil_img.info
+                revert_img_info(None, image[:-4] + f".{format}", pnginfo)
+        elif format_ == "jpg":
+            pass
+
+    logger.success("压缩完成!")
+    return "压缩完成!"
+
+
+def compression_and_organization(format_, image_path):
+    image_compression("png", image_path)
+    image_organization(image_path)
+    return "处理完成!"
